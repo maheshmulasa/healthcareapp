4d0cc67f80612a4bf294f8181c28b0d4baf8a4e0
Phanikumar <Phanikumar@Phanikumar-PC>
2019-09-08 19:00:43 +0530

    Commited & merged

--- src/main/java/com/healthcare/healthapp/HealthController.java
+++ src/main/java/com/healthcare/healthapp/HealthController.java
@@ -4,5 +4,23 @@ public class HealthController {
 	public HealthController() {
 		// TODO Auto-generated constructor stub
 	}
-
+	public Integer getdata()
+	  {
+	     return new Integer(2);
+	  }
+	
+	
+	public float getFlaot()
+	  {
+	     return  1.1f;
+	     
+	  }
+  public String getString()
+  {
+     return "System Updated";
+  }
+	public int getint()
+  {
+     return 2;
+  }
 }
